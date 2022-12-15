a49802d83842d34a623573e5f632382a258fc5e4
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2022-12-15 17:27:23 +0100

    Move todo to own file

--- test/main.c
+++ test/main.c
@@ -1,10 +1,3 @@
-// TODO:
-// [x] Print start and end of test suite
-// [ ] 1 suite with 1 case
-// [ ] 1 passing test
-// [ ] 1 failing test with EXPECT_EQ
-// [ ] print args to EXPECT_EQ on failure
-
 #include "r2k_test/r2k_test.h"
 
 #include <stdio.h>
---
+++ todo.md
@@ -0,0 +1,7 @@
+## TODO:
+
+- [x] Print start and end of test suite
+- [ ] 1 suite with 1 case
+- [ ] 1 passing test
+- [ ] 1 failing test with EXPECT_EQ
+- [ ] print args to EXPECT_EQ on failure
