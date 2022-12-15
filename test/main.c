cada3a957022852f8eed36e0e2df97e269e48dda
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2022-12-15 18:42:15 +0100

    Add start_test_case

--- test/main.c
+++ test/main.c
@@ -11,8 +11,9 @@ typedef struct test_case {
 } test_case_t;
 
 typedef struct test_suite {
-    test_case_t current_test;
+    const char* name;
     size_t num_ran_tests;
+    test_case_t current_test;
 } test_suite_t;
 
 void print_case_result(const test_case_t* test) {
@@ -20,45 +21,36 @@ void print_case_result(const test_case_t* test) {
     printf("%s.%s (0 ms)\n", test->suite, test->name);
 }
 
+void start_test_case(r2k_test_runner_t* runner, test_suite_t* suite, const char* case_name) {
+    if (suite->num_ran_tests > 0) {
+        print_case_result(&suite->current_test);
+    }
+
+    suite->num_ran_tests += 1;
+    suite->current_test.name = case_name;
+
+    printf_green("[ RUN      ] ");
+    printf("%s.%s\n", suite->name, suite->current_test.name);
+    runner->num_tests += 1;
+}
+
 void dummy_tests(r2k_test_runner_t* runner) {
     runner->num_suites += 1;
     test_suite_t suite = (test_suite_t) {
+        .name = __func__,
+        .num_ran_tests = 0,
         .current_test = {
             .suite = __func__,
             .name = NULL,
             .successful = false,
         },
-        .num_ran_tests = 0,
     };
 
     printf_green("[----------] ");
     printf("Running tests from %s\n", __func__);
 
-    {
-        if (suite.num_ran_tests > 0) {
-            print_case_result(&suite.current_test);
-        }
-
-        suite.num_ran_tests += 1;
-        suite.current_test.name = "hello";
-
-        printf_green("[ RUN      ] ");
-        printf("%s.%s\n", __func__, suite.current_test.name);
-        runner->num_tests += 1;
-    }
-
-    {
-        if (suite.num_ran_tests > 0) {
-            print_case_result(&suite.current_test);
-        }
-
-        suite.num_ran_tests += 1;
-        suite.current_test.name = "world";
-
-        printf_green("[ RUN      ] ");
-        printf("%s.%s\n", __func__, suite.current_test.name);
-        runner->num_tests += 1;
-    }
+    start_test_case(runner, &suite, "hello");
+    start_test_case(runner, &suite, "world");
 
     print_case_result(&suite.current_test);
 
