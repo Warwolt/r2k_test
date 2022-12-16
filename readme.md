301e65df47e73aec9960226e6eaad58d8f8b8559
Rasmus Källqvist <rasmus.kallqvist@gmail.com>
2022-12-16 23:17:25 +0100

    Update readme

--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
 # R2K Test
 
-A small test library for C programs, written to closely mimick [Google Test](https://github.com/google/googletest). Tests in R2K Test are defined with a `TEST()` macro, that are invoked inside of a function, that then becomes the test suite for the tests. These test suites are then ran by calling them from a test runner program.
+A small test library for C programs, written to closely mimick [Google Test](https://github.com/google/googletest). This is intended as a library that is lightweight and easy to integrate into a C project to add testing capabilities.
+
+Tests in R2K Test are defined with a `TEST()` macro, and the tests are defined inside of a function that becomes the test suite for the tests. These test suites are then ran by calling the corresponding functions from a test runner program.
 
 ```C
 #include <r2k_test/r2k_test.h>
@@ -28,7 +30,11 @@ int main(int argc, char** argv) {
 }
 ```
 
-Running this on the command line gives the familiar
+Running this on the command line gives an output that is very familiar to someone who's used Google Test:
+
+![arithmetic_tests_example](docs/arithmetic_tests.png)
+
+Note that the function `void arithmetic_tests()` becomes the test suite name, as seen in e.g. the `arithmetic_tests.addition_is_commutative` output.
 
 ## Build instructions
 (todo)
--- test/main.c
+++ test/main.c
@@ -1,56 +1,14 @@
-#include "r2k_test/r2k_test.h"
+#include <r2k_test/r2k_test.h>
 
-#include <string.h>
-
-void dummy_tests(void) {
+void arithmetic_tests() {
     TEST_SUITE_START();
 
-    TEST(expect_integers_ok) {
-        EXPECT_EQ(2 + 2, 4);
+    TEST(addition_is_commutative) {
+        EXPECT_EQ(2 + 3, 3 + 2);
     }
 
-    TEST(expect_integers_fail) {
-        EXPECT_EQ(2 + 2, 5);
-    }
-
-    TEST(expect_chars) {
-        EXPECT_CHAR_EQ('A', 'a');
-    }
-
-    TEST(expect_pointers) {
-        int a, b;
-        EXPECT_PTR_EQ(&a, &b);
-    }
-
-    TEST(expect_strings) {
-        EXPECT_STR_EQ("foo", "bar");
-    }
-
-    TEST(expect_float) {
-        EXPECT_FLOAT_NEAR(1.0f, 2.0f, 0.01);
-    }
-
-    TEST(expect_double) {
-        EXPECT_DOUBLE_NEAR(1.0, 2.01, 0.01);
-    }
-
-    TEST(expect_true) {
-        bool five_is_even = 5 % 2 == 0;
-        EXPECT_TRUE(five_is_even);
-    }
-
-    TEST_SUITE_END();
-}
-
-void other_tests(void) {
-    TEST_SUITE_START();
-
-    TEST(DISABLED_strcmp_equal_strings) {
-        EXPECT_TRUE(strcmp("aaa", "aaa") == 0);
-    }
-
-    TEST(strcmp_unequal_strings) {
-        EXPECT_TRUE(strcmp("aaa", "bbb") == 0);
+    TEST(zero_is_additive_identity) {
+        EXPECT_EQ(2 + 0, 2);
     }
 
     TEST_SUITE_END();
@@ -59,8 +17,7 @@ void other_tests(void) {
 int main(int argc, char** argv) {
     r2k_test_start(argc, argv);
 
-    dummy_tests();
-    other_tests();
+    arithmetic_tests();
 
     return r2k_test_end();
 }
